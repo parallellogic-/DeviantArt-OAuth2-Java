32e09e620728786929dd9976ce2e560ae9f7b919
Karson Kimbrel <karson.kimbrel@gmail.com>
2014-12-21 21:55:25 -0500

    Organized OAuth2 demos

--- src/com/kimbrelk/da/Main.java
+++ src/com/kimbrelk/da/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public final class Main {
 	private final static String URI_REDIRECT = "http://127.0.0.1/";
-	private final static AuthGrantType GRANT_TYPE = AuthGrantType.CLIENT_CREDENTIALS;
+	private final static AuthGrantType GRANT_TYPE = AuthGrantType.AUTHORIZATION_CODE;
 	
 	public final static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -48,17 +48,56 @@ public final class Main {
 		}
 		else {
 			// You are now authenticated and you can now do your OAuth2 requests
-			demoGetTokens(oAuth2);
+			
+			// Auth Demos
+			demoAuthGetTokens(oAuth2);
+			//demoAuthRevoke(oAuth2);
+			
+			// Browse Demos
+			// TODO
+			
+			// Collections Demos
+			// TODO
+			
+			// Comment Demos
+			// TODO
+			
+			// Curated Demos
+			// TODO
+			
+			// Deviation Demos
+			// TODO
+			
+			// Feed Demos
+			// TODO
+			
+			// Gallery Demos
+			// TODO
+			
+			// Sta.sh Demos
+			// TODO
+			
+			// User Demos
+			// TODO
 			demoUserWhoami(oAuth2);
 			demoUserWhois(oAuth2, "pickley");
-			demoAuthRevoke(oAuth2);
+			
+			// Util Demos
+			demoUtilPlacebo(oAuth2);
 		}
 		
 		in.close();
 	}
 	
+	private final static void demoAuthGetTokens(OAuth2 oAuth2) {
+		System.out.println("demoAuthGetTokens()");
+		System.out.println("Your access token:  " + oAuth2.getToken().getToken());
+		System.out.println("Your refresh token: " + oAuth2.getToken().getRefreshToken());
+		System.out.println();
+	}
 	private final static void demoAuthRevoke(OAuth2 oAuth2) {
 		// Revoke access token and force user reauthorization
+		System.out.println("demoAuthRevoke()");
 		Response resp = oAuth2.requestAuthRevoke();
 		if (resp.isSuccess()) {
 			System.out.println("Your authorization has been revoked.");
@@ -69,31 +108,43 @@ public final class Main {
 		}
 		System.out.println();
 	}
-	private final static void demoGetTokens(OAuth2 oAuth2) {
-		System.out.println("Your access token:  " + oAuth2.getToken().getToken());
-		System.out.println("Your refresh token: " + oAuth2.getToken().getRefreshToken());
-		System.out.println();
-	}
+	
 	private final static void demoUserWhoami(OAuth2 oAuth2) {
+		System.out.println("demoUserWhoami()");
 		Response resp;
 		resp = oAuth2.requestUserWhoami();
 		if (resp.isSuccess()) {
 			System.out.println("Your username is " + ((RespUserWhoami)resp).getUser().getName() + ".");
 		}
 		else {
-			System.out.println("Failed to get user info:");
+			System.out.println("Failed to get your user info:");
 			System.out.println(resp);
 		}
 		System.out.println();
 	}
 	private final static void demoUserWhois(OAuth2 oAuth2, String username) {
+		System.out.println("demoUserWhois()");
 		Response resp;
 		resp = oAuth2.requestUserWhois(username);
 		if (resp.isSuccess()) {
 			System.out.println(username + "'s real name is \'" + ((RespUserWhois)resp).getWhoisResults()[0].getRealName() + "\'.");
 		}
 		else {
-			System.out.println("Failed to get user info:");
+			System.out.println("Failed to get " + username + "\'s user info:");
+			System.out.println(resp);
+		}
+		System.out.println();
+	}
+	
+	private final static void demoUtilPlacebo(OAuth2 oAuth2) {
+		System.out.println("demoUtilPlacebo()");
+		Response resp;
+		resp = oAuth2.requestUtilPlacebo();
+		if (resp.isSuccess()) {
+			System.out.println("Your auth is valid.");
+		}
+		else {
+			System.out.println("Your auth is invalid:");
 			System.out.println(resp);
 		}
 		System.out.println();
